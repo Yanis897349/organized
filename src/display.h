5388fca5f06ce16268ea6dfdbd29438266a4b21f
Yanis <yanis.kernoua@epitech.eu>
2023-12-07 12:21:05 +0100

    feat(src): Helpers functions to display hardware

---
+++ src/display.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** organized [WSL: Ubuntu]
+** File description:
+** display
+*/
+
+#pragma once
+#include "organized.h"
+
+void display_added_hardware(material_t *hardware);
+void display_deleted_hardware(material_t *hardware);
+void display_hardware(material_t *hardware);
