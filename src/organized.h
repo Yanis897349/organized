a2a8297a72cadd6c09bc5e9561ebb3572fad3e10
Yanis <yanis.kernoua@epitech.eu>
2023-12-13 18:04:44 +0100

    feat(src): Support multiple sorting

--- src/organized.h
+++ src/organized.h
@@ -7,8 +7,15 @@
 
 #pragma once
 
+#include "include/my_list.h"
+
 typedef struct material_s {
     char *type;
     char *name;
     unsigned int id;
 } material_t;
+
+typedef struct {
+    int (*sort_function)(linked_list_t *, linked_list_t *, int);
+    int is_reverse;
+} sort_operation_t;
